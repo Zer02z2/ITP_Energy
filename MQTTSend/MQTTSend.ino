d96f260c89d14225d20220a59ba591557575b4fa
Zer02z2 <zongzechan@gmail.com>
2024-04-01 17:42:20 -0400

    LowPowerSend

--- MQTTSend/MQTTSend.ino
+++ MQTTSend/MQTTSend.ino
@@ -4,6 +4,7 @@
 #include <Adafruit_DPS310.h>
 #include <Wire.h>
 #include "ScioSense_ENS160.h"
+#include <Adafruit_SleepyDog.h>
 
 Adafruit_SHTC3 shtc3 = Adafruit_SHTC3(); // humidity 
 Adafruit_DPS310 dps;
@@ -11,8 +12,10 @@ Adafruit_Sensor *dps_pressure = dps.getPressureSensor();
 ScioSense_ENS160      ens160(ENS160_I2CADDR_1);
 
 float count = 0;
-int button = 2;
-int lastReading = 0;
+int sleepDuration = 4000;
+int sleepGoal = 40000;
+
+int sensorSwitch = 2;
 
 WiFiClient wifi;
 MqttClient mqtt(wifi);
@@ -35,39 +38,13 @@ const char pubTopic[] = "SolarWall_e/";
 const String subTopics[] = {"count/", "humidity/", "temp/", "pressure/", "AQI/", "TVOC/", "CO2/"};
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(sensorSwitch, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(sensorSwitch, HIGH);
   Serial.begin(115200);
 
-  // init SHTC3
-  Serial.println("SHTC3 test");
-  if (! shtc3.begin()) {
-    Serial.println("Couldn't find SHTC3");
-    while (1) delay(1);
-  }
-  Serial.println("Found SHTC3 sensor");
-
-  // init DPS310
-  Serial.println("DPS310");
-  if (! dps.begin_I2C()) {
-    Serial.println("Failed to find DPS");
-    while (1) yield();
-  }
-  dps.configurePressure(DPS310_64HZ, DPS310_64SAMPLES);
-  dps_pressure->printSensorDetails();
-  Serial.println("DPS OK!");
-
-  // init ENS160
-  Serial.print("ENS160...");
-  ens160.begin();
-  Serial.println(ens160.available() ? "done." : "failed!");
-  if (! ens160.available()) {
-    Serial.println("Fail to start ENS260");
-    while (1) delay(1);
-  }
-  Serial.print("\tRev: "); Serial.print(ens160.getMajorRev());
-  Serial.print("."); Serial.print(ens160.getMinorRev());
-  Serial.print("."); Serial.println(ens160.getBuild());
-  Serial.print("\tStandard mode ");
-  Serial.println(ens160.setMode(ENS160_OPMODE_STD) ? "done." : "failed!");
+  initSensors();
 
   Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
@@ -114,37 +91,87 @@ void loop() {
     mqtt.connect(broker, port);
     delay(200);
   }
-
-  if (true || digitalRead(button) == HIGH && lastReading == LOW) {
     
-    sensors_event_t humidity, temp, pressure_event;
-    shtc3.getEvent(&humidity, &temp);
-    dps_pressure->getEvent(&pressure_event);
-    ens160.measure(true);
-    ens160.measureRaw(true);
+  sensors_event_t humidity, temp, pressure_event;
+  shtc3.getEvent(&humidity, &temp);
+  dps_pressure->getEvent(&pressure_event);
+  ens160.measure(true);
+  ens160.measureRaw(true);
 
-    float values[] = {count,humidity.relative_humidity, temp.temperature, pressure_event.pressure,
-                      ens160.getAQI(), ens160.getTVOC(), ens160.geteCO2()};
-    String units[] = {"", " g/kg", " C", " hPa", "", "ppb", "ppm"};
+  float values[] = {count,humidity.relative_humidity, temp.temperature, pressure_event.pressure,
+                    ens160.getAQI(), ens160.getTVOC(), ens160.geteCO2()};
+  String units[] = {"", " g/kg", " C", " hPa", "", "ppb", "ppm"};
 
-    Serial.println("Starting sending MQTT...");
+  Serial.println("Starting sending MQTT...");
 
-    for (int i = 0; i < numOfTopics; i ++) {
-      String topicName = pubTopic + subTopics[i];
-      Serial.print("Topic: ");
-      Serial.print(topicName);
-      Serial.print(", Value: ");
-      Serial.print(values[i]);
-      Serial.println(units[i]);
-      mqtt.beginMessage(topicName);
-      mqtt.print(values[i]);
-      mqtt.print(units[i]);
-      mqtt.endMessage();
-    }
-
-    Serial.println("Complete sending");
-    count += 1;
+  for (int i = 0; i < numOfTopics; i ++) {
+    String topicName = pubTopic + subTopics[i];
+    Serial.print("Topic: ");
+    Serial.print(topicName);
+    Serial.print(", Value: ");
+    Serial.print(values[i]);
+    Serial.println(units[i]);
+    mqtt.beginMessage(topicName);
+    mqtt.print(values[i]);
+    mqtt.print(units[i]);
+    mqtt.endMessage();
   }
-  delay(60000);
-  lastReading = digitalRead(button);
+
+  Serial.println("Complete sending");
+  count += 1;
+  digitalWrite(sensorSwitch, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  for (int i = 0; i < sleepGoal; i += sleepDuration) {
+    // Serial.println("Going to sleep now");
+
+    int sleepMS = Watchdog.sleep(4000);
+
+    // ....... sleeping .......
+  }
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(sensorSwitch, HIGH);
+
+  Serial.print("I'm awake now! I slept for ");
+  Serial.print(sleepGoal);
+  Serial.println(" milliseconds.");
+  Serial.println();
+
+  initSensors();
+}
+
+void initSensors() {
+    // init SHTC3
+  Serial.println("SHTC3 test");
+  while (! shtc3.begin()) {
+    Serial.println("Couldn't find SHTC3");
+    delay(100);
+  }
+  Serial.println("Found SHTC3 sensor");
+
+  // init DPS310
+  Serial.println("DPS310");
+  while (! dps.begin_I2C()) {
+    Serial.println("Failed to find DPS");
+    delay(100);
+  }
+  dps.configurePressure(DPS310_64HZ, DPS310_64SAMPLES);
+  dps_pressure->printSensorDetails();
+  Serial.println("DPS OK!");
+
+  // init ENS160
+  Serial.print("ENS160...");
+  ens160.begin();
+  Serial.println(ens160.available() ? "done." : "failed!");
+  while (! ens160.available()) {
+    Serial.println("Fail to start ENS260");
+    ens160.begin();
+    delay(100);
+  }
+  Serial.print("\tRev: "); Serial.print(ens160.getMajorRev());
+  Serial.print("."); Serial.print(ens160.getMinorRev());
+  Serial.print("."); Serial.println(ens160.getBuild());
+  Serial.print("\tStandard mode ");
+  Serial.println(ens160.setMode(ENS160_OPMODE_STD) ? "done." : "failed!");
 }
